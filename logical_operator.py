0806d46c54c9e7e8fbdf83c1a152c744ee15552c
reddy bhrath <rompicherlareddybharath@gmail.com>
2026-09-08 13:31:14 +0530

    Add files via upload

---
+++ logical_operator.py
@@ -0,0 +1,14 @@
+# logical_operators.py for (and)
+a = 5
+result= a > 3 and a < 15
+print("result of ",a, ">", 3,"and", a, "<", 15,"is: ", result)
+
+# logical_operators.py for (or)
+b = 5
+result2 = b < 5 or b > 23
+print("result of ",b, "<", 5,"or", b, ">", 23,"is: ", result2)
+
+# logical_operators.py for (not)
+c = 5
+result3 = not(c > 3)
+print("result of not(",c, ">", 3,") is: ", result3)

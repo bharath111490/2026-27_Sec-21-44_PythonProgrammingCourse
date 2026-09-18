965399235f25d4fe72b72eb6569e7b936c1b3d39
reddy bhrath <rompicherlareddybharath@gmail.com>
2026-09-18 14:11:21 +0530

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,17 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+# Addition
+print("Sum of two numbers is:", a + b)
+# Subtraction
+print("Subtraction of two numbers is:", a - b)
+# Multiplication 
+print("Multiplication of two numbers is:", a * b)
+# Division
+print("Division of two numbers is:", a / b)
+# Floor division
+print("Floor division of two numbers is:", a // b)
+#percentage
+print("Remainder of two numbers is:", a % b)
+# Square
+print("Square of first number is:", a ** 2)
+print("Square of second number is:", b ** 2)

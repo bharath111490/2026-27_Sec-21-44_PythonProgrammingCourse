735f00889b54b6388a6c8a5124f08ef8b6398f7d
reddy bhrath <rompicherlareddybharath@gmail.com>
2026-09-20 15:23:27 +0530

    Refactor simple interest calculation and input prompts

--- simple_interest.py
+++ simple_interest.py
@@ -1,5 +1,6 @@
-principle=float(input("Enter the principle amount: "))
-rate=float(input("Enter the rate of interest:% "))
-time=float(input("Enter the time period: "))
-simple_intrest=(principle*rate*time)/100
-print("The simple interest is:",simple_intrest)
+Principle = float(input("Enter Principal amount: "))
+Rate = float(input("Enter Rate of Interest (%): "))
+Time = float(input("Enter Time (in years): "))
+
+simple_interest = (Principle * Rate * Time) / 100
+print("Simple Interest =", simple_interest)
